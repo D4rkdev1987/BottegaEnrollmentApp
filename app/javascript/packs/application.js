dacc50ac267d21225114da261be1a6b385c3613f
kadenjenson <sobeit99@gmail.com>
2018-06-18 09:13:57 -0600

    Figured out how import a react component with erb calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,39 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import React, { Component } from "react";
+// import Form from "./app/javascript/components/form.js";
+
 console.log('Hello World from Webpacker')
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
+
+class App extends Component {
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Form onChange={fields => this.onChange(fields)} />
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
